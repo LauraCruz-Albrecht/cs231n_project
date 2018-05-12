4b5fade36cacea31cea194cfb44a3d1ca51b4710
Deeksha <deeekshagoyal19@gmail.com>
2018-05-11 17:18:15 -0700

    submitting script

---
+++ master.py
@@ -0,0 +1,10 @@
+import csv
+
+
+# # convert training csv to array
+# results = []
+# with open("train.csv 4.30.14 PM.zip") as csvfile:
+#     reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC) # change contents to floats
+#     for row in reader: # each row is a list
+#         results.append(row)
+# results = results[1:]
